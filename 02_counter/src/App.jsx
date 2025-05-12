341742262992bb12bda1f2139519fb058cb31a3e
Ashutosh Singh <singhashutosh1025@gmail.com>
2025-05-12 17:37:50 +0530

    counter

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let[counter,setCounter]=useState(5);
+
+ const addValue=() =>{
+  // counter=counter+1;
+  // setCounter(counter);
+  if(counter<20){
+     setCounter(counter+1);
+  
+  }
+ 
+ }
+ const removeValue=()=>{
+  if(counter>0){
+    counter=counter-1;
+  setCounter(counter);
+  }
+  
+ }
+  return (
+    <>
+   <h1>chai aur react</h1>
+   <h2>couter value: {counter}</h2>
+ <button onClick={addValue}>Increase {counter}</button>
+ <br />
+ <button onClick={removeValue}>decrease {counter} </button>
+    </>
+  )
+}
+
+export default App
